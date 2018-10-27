dedd444849c51179fb87a1aea17f86790282324b
PC\Mihail Coscodan <mihail.coscodan@sapec.md>
2018-10-27 14:51:13 +0300

    Add gitIgnore file and factorial function with example of using it.

--- Program.pas
+++ Program.pas
@@ -1,6 +1,19 @@
 Program Lesson1_Program1;
+
+function fact(n: integer): longint;
+begin
+    if (n = 0) then
+        fact := 1
+    else
+        fact := n * fact(n - 1);
+end;
+var a : integer;
 Begin
+
 	Write('Hello World. Prepare to learn PASCAL!!');
-    if(5>4) then Write('5'); else Write('4');
+    {if(5>4) then Write('5'); else Write('4'\n);  }
+    { Example of using factorial function}
+    a := fact(5);
+    write(a);
 	Readln;
 End.
