a825344604cb948f147f86ddb4619ba28913f1f4
PC\Mihail Coscodan <mihail.coscodan@sapec.md>
2018-11-03 16:05:46 +0200

    Cond. probl. 4

--- Tablou ex 3.pas
+++ Tablou ex 3.pas
@@ -1,4 +1,6 @@
 Program p3;
 {Se citesc n elemente reale. Care este suma elementelor aflate pe pozitii impare in vector?
+ Se citesc numere reale pana la introducerea primului numar negativ care nu face parte din sir. Care este media elementelor din sir?
+
 
 }
