a04ca6be0330d145e2a913fcb15a463768e5f4a2
PC\Mihail Coscodan <mihail.coscodan@sapec.md>
2018-11-10 16:29:36 +0200

    add prob.7

--- problema5.pas
+++ problema5.pas
@@ -1,5 +1,6 @@
 {5. Se citesc n numere reale. Sa se afisese perechile de cate 3 numere aflate in vector pe pozitii succesive  care indeplinesc proprietatea ca al 3 este media primelor 2.   }
-{6. 6. Se citesc caractere pana la introducerea caracterului ‘*’. Cate vocale s-au citit?  }
+{6. Se citesc n numere gasiti tripletii (a,b,c) care satisfac legitatea a = b+c, b <c.
+{7 . Se citesc caractere pana la introducerea caracterului ‘*’. Cate vocale s-au citit?  }
  Program P5;
 
 
